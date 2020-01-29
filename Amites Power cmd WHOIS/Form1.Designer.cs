bbdaa5ca2833b4d75bde03a91ca40b4e2c8ff0e4
Tom Amite <59657309+Gitami@users.noreply.github.com>
2020-01-29 13:32:52 +0100

    Initial Commit. Moved from local to Github
    Files from 2009

---
+++ Amites Power cmd WHOIS/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace Amites_Power_cmd_WHOIS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.versionLabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Domainnames = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // versionLabel
+            // 
+            this.versionLabel.BackColor = System.Drawing.Color.Transparent;
+            this.versionLabel.Font = new System.Drawing.Font("Lucida Sans Unicode", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.versionLabel.ForeColor = System.Drawing.Color.White;
+            this.versionLabel.Location = new System.Drawing.Point(104, 442);
+            this.versionLabel.Name = "versionLabel";
+            this.versionLabel.Padding = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.versionLabel.Size = new System.Drawing.Size(69, 22);
+            this.versionLabel.TabIndex = 11;
+            this.versionLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(201, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(46, 29);
+            this.button1.TabIndex = 13;
+            this.button1.TabStop = false;
+            this.button1.Text = "Help";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.ForeColor = System.Drawing.Color.Black;
+            this.button2.Location = new System.Drawing.Point(247, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(52, 29);
+            this.button2.TabIndex = 14;
+            this.button2.TabStop = false;
+            this.button2.Text = "About";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.LayoutStyle = System.Windows.Forms.ToolStripLayoutStyle.HorizontalStackWithOverflow;
+            this.statusStrip1.Location = new System.Drawing.Point(0, 444);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(300, 22);
+            this.statusStrip1.SizingGrip = false;
+            this.statusStrip1.TabIndex = 15;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Transparent;
+            this.button4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button4.Font = new System.Drawing.Font("Verdana", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.Color.Black;
+            this.button4.Location = new System.Drawing.Point(9, 4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(192, 29);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Close ALL cmd";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button3.BackColor = System.Drawing.Color.Transparent;
+            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button3.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.Black;
+            this.button3.Location = new System.Drawing.Point(40, 393);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(216, 34);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Parse WHOIS";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.label3.Font = new System.Drawing.Font("Lucida Sans Unicode", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.DimGray;
+            this.label3.Location = new System.Drawing.Point(200, 448);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 14);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "2009  - by Amite";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.label4.Font = new System.Drawing.Font("Lucida Sans Unicode", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.DimGray;
+            this.label4.Location = new System.Drawing.Point(2, 448);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(96, 14);
+            this.label4.TabIndex = 20;
+            this.label4.Text = "Mass-CMD parser";
+            // 
+            // Domainnames
+            // 
+            this.Domainnames.BackColor = System.Drawing.Color.Black;
+            this.Domainnames.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Domainnames.CharacterCasing = System.Windows.Forms.CharacterCasing.Lower;
+            this.Domainnames.Font = new System.Drawing.Font("Courier New", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Domainnames.ForeColor = System.Drawing.Color.White;
+            this.Domainnames.Location = new System.Drawing.Point(12, 37);
+            this.Domainnames.Multiline = true;
+            this.Domainnames.Name = "Domainnames";
+            this.Domainnames.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.Domainnames.Size = new System.Drawing.Size(276, 350);
+            this.Domainnames.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            this.BackColor = System.Drawing.Color.LightSlateGray;
+            this.ClientSize = new System.Drawing.Size(300, 466);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.versionLabel);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.Domainnames);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.ForeColor = System.Drawing.Color.GreenYellow;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Domain to cmd-whois parser";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label versionLabel;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox Domainnames;
+
+    }
+}
+
---
+++ Amites Power cmd WHOIS/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+
+namespace Amites_Power_cmd_WHOIS
+{
+    public partial class Form1 : Form
+    {
+
+        // ###### VERSION TRACKING #############################
+            public static string versionNumber = "ver.:0.12-ALFA";
+            public static string versionBuild = "Build 2009112801";
+        // #####################################################
+
+
+        // Class constructor
+        public Form1()
+        {
+            InitializeComponent();
+            versionLabel.Text = "" + versionNumber;
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < Domainnames.Lines.Length; i++)
+            {
+                ProcessStartInfo ProcessInfo;
+                Process Process;
+
+                ProcessInfo = new ProcessStartInfo("cmd.exe", "/K " + "whois.exe " + Domainnames.Lines.GetValue(i).ToString());
+                Process = Process.Start(ProcessInfo);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            ProcessStartInfo ProcessInfo_CMDkill;
+            Process Process_CMDkill;
+            ProcessInfo_CMDkill = new ProcessStartInfo("cmd.exe", "/C " + "taskkill.exe " + "/F /IM cmd.exe /T");
+            Process_CMDkill = Process.Start(ProcessInfo_CMDkill);
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("\r\n" + "Power tool for CMD Whois" + "\r\n" + "by Amite. " + "\r\n\r\n" + versionNumber + " | " + versionBuild, "About");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Parses a list of domain names, line by line, to the Windows CMD prompt, where the "
+                            + "whois.exe will run once for each domain, i.e. spawning a DOS window with the output for each."
+                            + "You can use the KILL-button to termination all your CMD windows when you are done." + "\r\n\r\n"
+                            + "NOTE: whois.exe is NOT included with this tool. You will have to download (Freeware) that separately.",
+                              "Help");
+        }
+
+    }
+}
